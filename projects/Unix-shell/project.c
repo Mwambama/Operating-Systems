81556531c7453e0d28f1f2de1f9d0acddbd04be8
mwambama <mwambama@linuxvdi-f25-19.ece.iastate.edu>
2025-10-07 21:42:32 -0500

    project setup| main&infinite loop

---
+++ projects/Unix-shell/project.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h> 
+#include <stdlib.h>  
+#include <stdbool.h>
+#include <sys/wait.h>
+
+
+
+int main ( int argc, char **argv)
+{
+
+       char prompt = "308sh>";   // the user prompt
+
+
+       char command[1024];   // infinite loop acceting ipunt from user
+       while (1)
+       {
+        /* code */
+     if ( strcmp(command, "exist") == 0){
+
+     }
+       }
+       
+}
+
+
+
